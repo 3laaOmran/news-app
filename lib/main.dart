2f7f10d47a552a4ac98322983f12bec089ed5af1
3laaOmran <115973437+3laaOmran@users.noreply.github.com>
2025-01-14 17:30:24 +0200

    Initial Project Added splash screen.

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:news_app/ui/home_screen/home_screen.dart';
+import 'package:news_app/utils/app_theme.dart';
+
+void main() {
+  runApp(const NewsApp());
+}
+
+class NewsApp extends StatelessWidget {
+  const NewsApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: HomeScreen.routeName,
+      routes: {
+        HomeScreen.routeName: (context) => const HomeScreen(),
+      },
+      theme: AppTheme.lightTheme,
+      darkTheme: AppTheme.darkTheme,
+      themeMode: ThemeMode.light,
+    );
+  }
+}
